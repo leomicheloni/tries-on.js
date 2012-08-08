85ae48e87d9ca221db43031268bafe3ebf59a0e7
diegokinetica <diego@kinetica-solutions.com>
2012-08-08 18:00:22 -0300

    - Pantalla simple que muestra el log de los requests recibidos

--- connectionServer.js
+++ connectionServer.js
@@ -1,25 +1,9 @@
 var http = require("http");
-var notifier = require('./lib/notifier').notifier;
 var requestDispatcher = require('./lib/requestDispatcher').requestDispatcher;
 
 http.createServer(function (request, response) {
 	request.setEncoding("utf-8");
 	
-	notifier.notify(request.url);
-	requestDispatcher.dispatch(request);
-	
-	//notifier.report();
-	
-	
-	/*
-	var req = http.request({ hostname: urlCliente.hostname, port: urlCliente.port, method: 'GET', path: '/?ok' }, function(res) {
-		console.log('se envio el request al cliente ' + urlClienteRaw);
-	});
-	req.on('error', function(e) {
-	  console.log('problem with request: ' + e.message);
-	});
-	req.end();
-	*/
-	response.end();
+	requestDispatcher.dispatch(request, response);
  })
 .listen(3001);

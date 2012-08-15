d6cb7dc78bf2521d761400f78ec77a57a543e9f8
diegokinetica <diego@kinetica-solutions.com>
2012-08-15 16:47:25 -0300

    - Se agrego un manejo mas simple para las urlsy despachos.

--- connectionServer.js
+++ connectionServer.js
@@ -1,5 +1,13 @@
 var http = require("http");
-var requestDispatcher = require('./lib/requestDispatcher').requestDispatcher;
+var RD = require('./lib/requestDispatcher');
+var requestDispatcher = RD.requestDispatcher;
+
+requestDispatcher.addHandler(
+	new RD.UrlHandler('/test/.*', function(request, response){
+		response.write('test');
+		response.end();
+	})
+);
 
 http.createServer(function (request, response) {
 	request.setEncoding("utf-8");
